114f13e10dabd0ff1911fb4ac6cb3945cde48e0f
yury <hmelevskih94@gmail.com>
2022-05-22 08:33:07 +0300

    Created Main Screen

---
+++ app/src/main/java/com/example/variant11app/di/ViewModelModule.kt
@@ -0,0 +1,9 @@
+package com.example.variant11app.di
+
+import com.example.variant11app.presentation.main.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel { MainViewModel() }
+}
